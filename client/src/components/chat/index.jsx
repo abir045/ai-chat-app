398c5acb7a0940c2b162bd2d25e655f9b05503d6
Salekin Imran <abir045@gmail.com>
2023-07-04 19:03:23 +0600

    initial commit

---
+++ client/src/components/chat/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { useMultiChatLogic, MultiChatSocket, MultiChatWindow } from 'react-chat-engine-advanced'
+import Header from '../customHeader'
+
+
+const Chat = () => {
+
+const chatProps = useMultiChatLogic(
+    import.meta.env.VITE_PROJECT_ID,
+    "testuser",
+    "1234"
+)
+
+   return (
+
+    <div style={{ flexBasis: "100%" }} >
+
+   <MultiChatSocket {...chatProps} />
+    <MultiChatWindow 
+    {...chatProps} 
+    renderChatHeader={(chat) => <Header chat={chat} />}
+    
+    />
+    
+    </div>
+    
+    )
+}
+
+export default Chat
